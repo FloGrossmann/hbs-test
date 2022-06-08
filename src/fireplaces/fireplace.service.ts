6a7507ca47c85c3871946b608aea3f86d4080b18
florian <florian4774@gmail.com>
2022-06-08 20:54:34 +0200

    feat(): magic stone slabs

--- src/fireplaces/fireplace.service.ts
+++ src/fireplaces/fireplace.service.ts
@@ -7,14 +7,15 @@ import { v4 } from 'uuid';
 export interface IFirePlace {
   value: boolean,
   class: string,
-  index: string
+  uuid: string,
+  slabClass: string
 }
 
 @Injectable()
 export class FirePlaceService {
   firePlaces: IFirePlace[] = [];
-  indexMap: Map<string,number> = new Map();
-  numberOfFirePots = 14;
+  indexMap: Map<string, number> = new Map();
+  numberOfFirePots = 6;
   finished: boolean = false;
   @WebSocketServer() server: Server;
 
@@ -38,11 +39,11 @@ export class FirePlaceService {
       this.indexMap.set(id, i);
       this.firePlaces.push({
         value: on,
-        index: id,
-        class: on ? 'firepot firepot-on' : 'firepot firepot-off'
+        uuid: id,
+        class: on ? 'firepot firepot-on' : 'firepot firepot-off',
+        slabClass: 'slab-on'
       });
     }
-    console.log(this.indexMap)
     this.shuffleArray(this.firePlaces);
     this.firePlaces.reverse();
     this.shuffleArray(this.firePlaces);
@@ -51,10 +52,10 @@ export class FirePlaceService {
   /* Randomize array in-place using Durstenfeld shuffle algorithm */
   shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
   }
 
@@ -68,16 +69,40 @@ export class FirePlaceService {
       return this.firePlaces;
     }
     let index = this.indexMap.get(id);
-    // Go to the given index, then flip every boolean value after it
+
+    // get the selected firepot
     for (let fireplace of this.firePlaces) {
-      if (this.indexMap.get(fireplace.index) >= index) {
+      if (fireplace.uuid === id) {
+        if (fireplace.slabClass === 'slab-off') {
+          // Dont flip if already off
+          return this.firePlaces;
+        } else {
+          fireplace.slabClass = 'slab-off';
+        }
+      }
+    }
+
+    // Check: If all slabs are off, turn all of them on again
+    let turnOnSlabs = true;
+
+    for (let fireplace of this.firePlaces) {
+      if (fireplace.slabClass === 'slab-on') {
+        turnOnSlabs = false;
+      }
+      // Go to the given index, then flip every boolean value after it
+      if (this.indexMap.get(fireplace.uuid) >= index) {
         this.toggleFirePlace(fireplace);
       }
     }
+
+    if (turnOnSlabs) {
+      this.firePlaces.forEach(fireplace => fireplace.slabClass = 'slab-on');
+    }
+
     this.firePlaceSendGateway.send(this.firePlaces);
     if (this.checkIfWon()) {
       this.firePlaceSendGateway.sendWon();
-    } 
+    }
   }
 
   checkIfWon() {
@@ -90,6 +115,12 @@ export class FirePlaceService {
   }
 
   getFirePlaces() {
+    // New connections need to know the game is over
+    setTimeout(() => {
+      if (this.checkIfWon()) {
+        this.firePlaceSendGateway.sendWon();
+      }
+    },200)
     return this.firePlaces;
   }
 }
